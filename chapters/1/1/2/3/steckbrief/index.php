1902029a803451070233e4d9af802594c3570736
Sneaker2000 <vl@lamke.de>
2022-09-10 13:31:42 +0200

    HA1: Steckbrief

---
+++ chapters/1/1/2/3/steckbrief/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Steckbrief</title>
+  </head>
+  <body>
+    <h1>Steckbrief - Valentin Lamke</h1>
+    <p>Mein Name ist Valentin, ich komme aus Mainz und gehe auf das Gymnasium Oberstadt.</p>
+    <p>Ich interessiere mich für Informatik und Musik.</p>
+    <p>Hier stehe ich vor einer Orgel:</p>
+    <img src="img1.jpeg" width="190" title="Hier stehe ich vor einer Orgel">
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<DOCTYPE html>
-<htm></html>
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Info 2022</title>
+  </head>
+  <body>
+    <h1>Informatik 2022</h1>
+    <h2><a href="https://www.inf-schule.de/information"># 1: Informationen und ihre Darstellung</a></h2>
+    <h3><a href="https://www.inf-schule.de/information/informationsdarstellunginternet"># 1.1: Informationsdarstellung im Internet</a></h3>
+    <h4><a href="https://www.inf-schule.de/information/informationsdarstellunginternet/strukturbeschreibunghtml"># 1.1.2: Strukturbeschreibung mit HTML</a></h4>
+    <h5><a href="https://www.inf-schule.de/information/informationsdarstellunginternet/strukturbeschreibunghtml/miniprojekt_steckbrief"># 1.1.2.3: Miniprojekt - Steckbrief</a></h5>
+    <a href="chapters/1/1/2/3/steckbrief/">Aufgabe 1: Steckbrief</a>
+  </body>
+</html>
