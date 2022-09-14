921e6c2120f35afedcb51c39dcb5d4d288b701ba
Sneaker2000 <vl@lamke.de>
2022-09-14 15:41:09 +0200

    New design

--- chapters/1/1/2/3/steckbrief/index.php
+++ chapters/1/1/2/3/steckbrief/index.php
@@ -3,12 +3,20 @@
   <head>
     <meta charset="utf-8">
     <title>Steckbrief</title>
+    <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <h1>Steckbrief - Valentin Lamke</h1>
+    <h1>Steckbrief - Valentin Lamke</h1><br>
     <p>Mein Name ist Valentin, ich komme aus Mainz und gehe auf das Gymnasium Oberstadt.</p>
-    <p>Ich interessiere mich für Informatik und Musik.</p>
+    <p>Ich interessiere mich für Informatik und Musik.</p><br>
     <p>Hier stehe ich vor einer Orgel:</p>
     <img src="img1.jpeg" width="190" title="Hier stehe ich vor einer Orgel">
+
+    <p>Ich programmiere gerne mit:</p>
+    <ul>
+      <li>HTML</li>
+      <li>PHP</li>
+      <li>CSS</li>
+    </ul>
   </body>
 </html>
