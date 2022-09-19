0a575dae89ec511c726b433813ce381646cde454
Sneaker2000 <vl@lamke.de>
2022-09-19 20:04:07 +0200

    linking

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
     <h3><a href="https://www.inf-schule.de/information/informationsdarstellunginternet"># 1.1: Informationsdarstellung im Internet</a></h3>
     <h4><a href="https://www.inf-schule.de/information/informationsdarstellunginternet/strukturbeschreibunghtml"># 1.1.2: Strukturbeschreibung mit HTML</a></h4>
     <h5><a href="https://www.inf-schule.de/information/informationsdarstellunginternet/strukturbeschreibunghtml/miniprojekt_steckbrief"># 1.1.2.3: Miniprojekt - Steckbrief</a></h5>
-    <a href="chapters/1/1/2/3/steckbrief/">Aufgabe 1: Steckbrief</a>
+    <a href="chapters/1/1/2/3/steckbrief/1/">Aufgabe 1: Steckbrief</a>
   </body>
 </html>
